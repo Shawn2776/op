63db6404904b8efa9a10684e838e90c76df4382d
sdharrin <138178989+sdharrin@users.noreply.github.com>
2024-11-20 11:03:39 -0800

    hero work

--- components/Home/Hero.jsx
+++ components/Home/Hero.jsx
@@ -1,20 +1,30 @@
 import React from "react";
+import { Button } from "../ui/button";
+import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="hero">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <img src="/op_hero2.jpg" className="max-w-sm rounded-lg shadow-2xl" />
-        <div>
-          <h1 className="text-5xl font-bold">Watersport Rentals</h1>
-          <p className="py-6">
-            NIC Adventure Center, with over 1,000 feet of beachfront, offers
-            convenient rentals and activities like kayaking, paddleboarding, and
-            sailing, providing seamless adventures for students and the
-            community.
-          </p>
-          <button className="btn btn-primary">Rent Now</button>
+    <div className="grid grid-cols-1 md:grid-cols-2">
+      <div>
+        <h2 className="text-2xl md:6xl font-bold">Watersport Rentals</h2>
+        <h3 className="text-lg md:text-4xl text-gray-500 pr-20 mt-5">
+          NIC Adventure Center offers convenient rentals and activities like
+          kayaking, paddleboarding, and sailing.
+        </h3>
+        <div className="pb-4 pt-2 mt-5 px-4">
+          <Button>
+            <span className="text-xl">Rent Now</span>
+          </Button>
         </div>
+      </div>
+      <div>
+        <Image
+          src="/op_hero3.jpg"
+          alt="Hero Image"
+          height={400}
+          width={500}
+          className="w-full object-cover align-middle "
+        />
       </div>
     </div>
   );
