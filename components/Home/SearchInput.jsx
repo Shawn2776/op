a203cfcda7c1f8ab99202a95703943767557c5d7
sdharrin <138178989+sdharrin@users.noreply.github.com>
2024-11-20 12:54:59 -0800

    search input calendar

--- components/Home/SearchInput.jsx
+++ components/Home/SearchInput.jsx
@@ -5,24 +5,26 @@ import { Input } from "@/components/ui/input";
 const SearchInput = () => {
   return (
     <div>
-      <h2 className="text-center text-[20px] text-gray-400 mb-4">
+      <h2 className="text-center text-[20px] text-gray-400 mb-4 mt-5">
         Plan Your Next Adventure
       </h2>
-      <div className="flex gap-10 justify-center">
-        <div className="flex items-center gap-2">
-          <IoLocationSharp size={30} className="text-gray-400" />
-          <Input
-            type="text"
-            placeholder="Location"
-            className="p-2 outline-none bg-white"
-          />
-        </div>
-        <div className="flex items-center gap-2">
-          <FaRegCalendarAlt size={30} className="text-gray-400" />
-          <Input
-            type="date"
-            className="p-2 outline-none bg-white text-gray-400"
-          />
+      <div className="flex gap-24 justify-center">
+        <div className="flex bg-gray-100 gap-10 divide-x-2 p-2 px-5 rounded-full">
+          <div className="flex items-center gap-2">
+            <IoLocationSharp size={30} className="text-gray-400" />
+            <Input
+              type="text"
+              placeholder="Location"
+              className="p-2 outline-none bg-white"
+            />
+          </div>
+          <div className="flex items-center gap-2 pl-10">
+            <FaRegCalendarAlt size={30} className="text-gray-400" />
+            <input
+              type="date"
+              className="p-2 outline-none bg-white text-gray-400"
+            />
+          </div>
         </div>
       </div>
     </div>
